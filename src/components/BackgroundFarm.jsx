3e06c43e49fb2532f98f2fe881bafb7303ad1abe
said-boy <alkhudrimsaid733@gmail.com>
2024-11-07 18:03:58 +0700

    update animasi background

--- src/components/BackgroundFarm.jsx
+++ src/components/BackgroundFarm.jsx
@@ -2,8 +2,8 @@ import { motion } from "framer-motion";
 
 export default function BackgroundFarm() {
   return (
-    <div 
-      className="bg-[#F4F9F4] fixed top-0 left-0 w-full h-full z-0" 
+    <div
+      className="bg-[#F4F9F4] fixed top-0 left-0 w-full h-full z-0"
       style={{ pointerEvents: 'none' }} // Agar background tidak menghalangi interaksi dengan konten lainnya
     >
       <motion.div
@@ -15,18 +15,18 @@ export default function BackgroundFarm() {
           repeat: Infinity,
           repeatType: "loop",
         }}
-        className="absolute top-10 left-40 h-[30rem] w-[30rem] bg-[#A7D7C5] rounded-full blur-[400px]"
+        className="absolute top-10 left-40 h-[40rem] w-[40rem] bg-[#A7D7C5] rounded-full blur-[1500px] opacity-70"
       ></motion.div>
       <motion.div
         initial={{ right: "224px" }}
-        animate={{ right: ["224px", "600px", "224px"] }}
+        animate={{ right: ["224px", "800px", "224px"], top: ["450px", "450px", "0px", "450px"], }}
         transition={{
           duration: 10,
           ease: "easeInOut",
           repeat: Infinity,
           repeatType: "loop",
         }}
-        className="absolute top-[450px] right-56 h-96 w-96 bg-[#FF9F43] rounded-full blur-[400px]"
+        className="absolute top-[450px] right-56 h-[30rem] w-[30rem] bg-[#FF9F43] rounded-full blur-[1500px] opacity-70"
       ></motion.div>
       <motion.div
         initial={{ top: "20px" }}
@@ -37,7 +37,7 @@ export default function BackgroundFarm() {
           repeat: Infinity,
           repeatType: "loop",
         }}
-        className="absolute top-5 right-28 h-96 w-96 bg-[#7367F0] rounded-full blur-[400px]"
+        className="absolute top-5 right-28 h-[30rem] w-[30rem] bg-[#7367F0] rounded-full blur-[1500px] opacity-70"
       ></motion.div>
     </div>
   );
