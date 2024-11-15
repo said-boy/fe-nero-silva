abe06f0744046cb27de41951b7a52e85c894704f
said-boy <saiboy@gmail.com>
2024-11-15 08:55:41 +0700

    change background in auth page

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import Background from "../components/Backgournd";
+import Background from "../components/BackgroundFarm";
 import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
 
@@ -7,8 +7,8 @@ export default function AuthPage() {
   const { action } = useParams();
   return (
     <>
-      <div className="relative">
-        <Background />
+      <Background />
+      <div className="relative pt-5 font-HelveticaNeueRoman max-w-screen-xl mx-auto">
         {action === "signin" ? (
           <div className="absolute top-0">
             <SignIn to={"/auth/signup"} />
